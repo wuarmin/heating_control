bfa2f1f77e864cd5bc96e298a725e2c0fa9943ed
armin <armin@wurzweb.com>
2017-12-03 12:16:57 +0100

    CHG: add new item to .gitignore

--- lib/interactors/control_pump.py
+++ lib/interactors/control_pump.py
@@ -1,12 +1,28 @@
-from ..repositories.pump_control_rule_repository import PumpControlRuleRepository
+from ..repositories.pump_schedule_repository import PumpScheduleRepository
 
 class ControlPump(object):
 
   def __init__(self):
-    self.pump_control_repo = PumpControlRuleRepository()
+    self.pump_schedule_repo = PumpScheduleRepository()
 
   def __call__(self, pump, current_time):
     self.pump = pump
+    self.current_time = current_time
+    next_schedule = self.next_schedule()
 
-  def rule(self):
-    return self.pump_control_repo.find_for_pump_id(self.pump.id)
+    print("here next_schedule", next_schedule);
+
+    # next_schedule = rule.next_schedule(pump, current_time)
+    # print("next_schedule", next_schedule.next_start)
+
+    # get the current temperature
+    # what's the current time slice
+    # when is next start and next stop
+
+  def next_schedule(self):
+    next_schedule = self.pump_schedule_repo.find_by_pump_id(self.pump.id)
+    if(next_schedule is None):
+      rule = self.pump.rule
+      next_schedule = rule.next_schedule(self.pump, self.current_time)
+      self.pump_schedule_repo.create(next_schedule)
+    return next_schedule
