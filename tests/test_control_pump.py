75d8adadfcde3be362d3d58d631a02bb748a72a9
armin <armin@wurzweb.com>
2018-07-31 20:09:04 +0200

    CHG: test all scenarios at ControlPump-Interactor

--- tests/test_control_pump.py
+++ tests/test_control_pump.py
@@ -80,6 +80,40 @@ def doubles_switch_off_pump_if_next_schedule_is_none(current_time, pump):
     Doubles = namedtuple('Doubles' , 'pump_schedule_repo get_next_pump_schedule')
     return Doubles(pump_schedule_repo, get_next_pump_schedule)
 
+
+
+
+def test_switch_off_pump_if_current_time_is_before_next_start(pump):
+    current_time = datetime(1990, 12, 24, 23, 30, 59, 000000)
+
+    doubles = doubles_switch_off_pump_if_current_time_is_before_next_start(current_time, pump)
+
+    control_pump = ControlPump(
+        pump_schedule_repo=doubles.pump_schedule_repo,
+        get_next_pump_schedule=doubles.get_next_pump_schedule
+    )
+    control_pump(current_time=current_time, pump=pump)
+    
+    assert pump.off == True
+
+def doubles_switch_off_pump_if_current_time_is_before_next_start(current_time, pump):
+    pump_schedule_repo = InstanceDouble('lib.repositories.pump_schedule_repository.PumpScheduleRepository')
+    next_schedule = PumpSchedule(
+        pump_id=pump.id,
+        next_start=datetime(1990, 12, 24, 23, 40, 59, 000000),
+        next_stop=datetime(1990, 12, 25, 1, 00, 00, 000000)
+    )
+    expect(pump_schedule_repo).find_by_pump_id('OG').and_return(next_schedule)
+
+    get_next_pump_schedule = InstanceDouble('lib.interactors.get_next_pump_schedule.GetNextPumpSchedule')
+
+    Doubles = namedtuple('Doubles' , 'pump_schedule_repo get_next_pump_schedule')
+    return Doubles(pump_schedule_repo, get_next_pump_schedule)
+
+
+
+
+
 def test_switch_off_pump_if_current_time_is_after_next_stop(pump):
     current_time = datetime(1990, 12, 24, 23, 30, 59, 000000)
 
@@ -99,6 +133,99 @@ def doubles_switch_off_pump_current_time_is_after_next_stop(current_time, pump):
         pump_id=pump.id,
         next_start=datetime(1990, 12, 24, 23, 00, 59, 000000),
         next_stop=datetime(1990, 12, 24, 23, 30, 58, 000000)
+    )
+    expect(pump_schedule_repo).find_by_pump_id('OG').and_return(next_schedule)
+    expect(pump_schedule_repo).delete(next_schedule)
+
+    get_next_pump_schedule = InstanceDouble('lib.interactors.get_next_pump_schedule.GetNextPumpSchedule')
+
+    Doubles = namedtuple('Doubles' , 'pump_schedule_repo get_next_pump_schedule')
+    return Doubles(pump_schedule_repo, get_next_pump_schedule)
+
+
+
+
+def test_switch_on_pump_if_current_time_is_between_next_start_and_stop(pump):
+    current_time = datetime(1990, 12, 24, 15, 30, 59, 000000)
+
+    doubles = doubles_switch_on_pump_if_current_time_is_between_next_start_and_stop(current_time, pump)
+
+    control_pump = ControlPump(
+        pump_schedule_repo=doubles.pump_schedule_repo,
+        get_next_pump_schedule=doubles.get_next_pump_schedule
+    )
+    control_pump(current_time=current_time, pump=pump)
+    
+    assert pump.on == True
+
+def doubles_switch_on_pump_if_current_time_is_between_next_start_and_stop(current_time, pump):
+    pump_schedule_repo = InstanceDouble('lib.repositories.pump_schedule_repository.PumpScheduleRepository')
+    next_schedule = PumpSchedule(
+        pump_id=pump.id,
+        next_start=datetime(1990, 12, 24, 15, 29, 59, 000000),
+        next_stop=datetime(1990, 12, 24, 16, 00, 58, 000000)
+    )
+    expect(pump_schedule_repo).find_by_pump_id('OG').and_return(next_schedule)
+
+    get_next_pump_schedule = InstanceDouble('lib.interactors.get_next_pump_schedule.GetNextPumpSchedule')
+
+    Doubles = namedtuple('Doubles' , 'pump_schedule_repo get_next_pump_schedule')
+    return Doubles(pump_schedule_repo, get_next_pump_schedule)
+
+
+
+
+def test_keep_pump_switched_off_if_current_time_is_before_next_start(pump):
+    pump.switch_off()
+    current_time = datetime(1990, 12, 24, 15, 28, 59, 000000)
+
+    doubles = doubles_keep_pump_switched_off_if_current_time_is_before_next_start(current_time, pump)
+
+    control_pump = ControlPump(
+        pump_schedule_repo=doubles.pump_schedule_repo,
+        get_next_pump_schedule=doubles.get_next_pump_schedule
+    )
+    control_pump(current_time=current_time, pump=pump)
+    
+    assert pump.off == True
+
+def doubles_keep_pump_switched_off_if_current_time_is_before_next_start(current_time, pump):
+    pump_schedule_repo = InstanceDouble('lib.repositories.pump_schedule_repository.PumpScheduleRepository')
+    next_schedule = PumpSchedule(
+        pump_id=pump.id,
+        next_start=datetime(1990, 12, 24, 15, 29, 59, 000000),
+        next_stop=datetime(1990, 12, 24, 16, 00, 58, 000000)
+    )
+    expect(pump_schedule_repo).find_by_pump_id('OG').and_return(next_schedule)
+
+    get_next_pump_schedule = InstanceDouble('lib.interactors.get_next_pump_schedule.GetNextPumpSchedule')
+
+    Doubles = namedtuple('Doubles' , 'pump_schedule_repo get_next_pump_schedule')
+    return Doubles(pump_schedule_repo, get_next_pump_schedule)
+
+
+
+
+def test_keep_pump_switched_off_and_delete_schedule_if_current_time_is_after_next_stop(pump):
+    pump.switch_off()
+    current_time = datetime(1990, 12, 24, 16, 1, 59, 000000)
+
+    doubles = doubles_keep_pump_switched_off_and_delete_schedule_if_current_time_is_after_next_stop(current_time, pump)
+
+    control_pump = ControlPump(
+        pump_schedule_repo=doubles.pump_schedule_repo,
+        get_next_pump_schedule=doubles.get_next_pump_schedule
+    )
+    control_pump(current_time=current_time, pump=pump)
+    
+    assert pump.off == True
+
+def doubles_keep_pump_switched_off_and_delete_schedule_if_current_time_is_after_next_stop(current_time, pump):
+    pump_schedule_repo = InstanceDouble('lib.repositories.pump_schedule_repository.PumpScheduleRepository')
+    next_schedule = PumpSchedule(
+        pump_id=pump.id,
+        next_start=datetime(1990, 12, 24, 15, 29, 59, 000000),
+        next_stop=datetime(1990, 12, 24, 16, 00, 58, 000000)
     )
     expect(pump_schedule_repo).find_by_pump_id('OG').and_return(next_schedule)
     expect(pump_schedule_repo).delete(next_schedule)
